6e81c0f3e68adb8fd3ccf32accdd261606dbae70
agh4m <tiagofigueiredo2003@gmail.com>
2024-12-16 16:05:29 +0000

    translate most documents

--- translate.py
+++ translate.py
@@ -1,10 +1,29 @@
-from threading import Lock, Thread
-
-import nltk
-import pandas as pd
+from threading import Thread
 from deep_translator import GoogleTranslator
+import nltk
+from pandas.core.api import DataFrame
+import numpy as np
+import pandas as pd
 
-THREAD_NUMBER = 48
+THREAD_NUMBER = 64
+
+
+class ReturningThread(Thread):
+
+    def __init__(self, group=None, target=None, name=None,
+                 args=(), kwargs={}, Verbose=None):
+        Thread.__init__(self, group, target, name, args, kwargs)
+        self._return = None
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args,
+                                        **self._kwargs)
+
+    def join(self, *args):
+        Thread.join(self, *args)
+        return self._return
+
 
 nltk.download("vader_lexicon")
 nltk.download("punkt_tab")
@@ -26,65 +45,69 @@ else:
 df["text"] = df["text"].str.strip().str.lower()
 df.drop_duplicates(subset="text", keep="first", inplace=True)
 
-translator = GoogleTranslator(source="pt", target="en")
 
-
-def translate_text(sentences, i):
-    print(f"#{i}: Translating {len(sentences)} sentences")
-    return translator.translate_batch(sentences)
+def translate_text(translator, sentences, t_n, tries):
+    print(f"#thread {t_n}: Translating {len(sentences)} sentences")
+    # print(sentences)
+    # return str(sentences)
+    for i in range(tries):
+        try:
+            tr = " ".join(translator.translate_batch(sentences))
+            print(tr)
+            return tr
+        except Exception as e:
+            print(f"thread {t_n}, try {i}, error {e}")
+    return None
 
 
 def spli_in_5000_sentences(text):
-    return [text[i : i + 4998] for i in range(0, len(text), 4998)]
+    return [text[i:i + 4998] for i in range(0, len(text), 4998)]
+    # return text.split(".")
 
 
 # Step 4: Translate the text
-df["sentences"] = df["text"].apply(spli_in_5000_sentences)
-df["translated_text"] = ""
-df.reset_index(drop=True)
-
-lock = Lock()
-
-
-def thread(begin: int, end: int, lock: Lock) -> None:
-    global df
-
-    for i in range(begin, end + 1):
-        try:
-            translated = translate_text(df["sentences"][i], i)
-
-            lock.acquire()
-            df["translated_text"][i] = translated
-            lock.release()
-        except KeyError:
-            print(i, "error")
+df['sentences'] = df['text'].apply(spli_in_5000_sentences)
+df['translated_text'] = ""
+# df["translated_text"] = df["sentences"].apply(lambda x: translate_text(x, 0))
+df.reset_index(drop=True, inplace=True)
+print(df.head())
 
 
-step = len(df) // THREAD_NUMBER
-missing = len(df) - step * THREAD_NUMBER
+def thread(df: pd.DataFrame, t_n: int) -> DataFrame:
+    translator = GoogleTranslator(source='pt', target='en')
+    print("running from thread ", i)
+    df["translated_text"] = df["sentences"].apply(lambda x: translate_text(translator, x, t_n, 3))
+    return df
+
+
+batches = np.array_split(df, THREAD_NUMBER)
+for i in batches:
+    print(i.__len__())
+
 threads: list[Thread] = []
 
 for i in range(0, THREAD_NUMBER):
-    t = Thread(target=thread, args=[i * step, i + 1 * step - 1, lock])
+    print("starting thread ", i)
+    t = ReturningThread(target=thread, args=[batches[i], i])
     t.start()
     threads.append(t)
 
-t = Thread(target=thread, args=[(THREAD_NUMBER) * step, len(df) - 1, lock])
-t.start()
-threads.append(t)
+print(len(threads))
+
+dfs = []
 
 for t in threads:
-    t.join()
+    df_1 = t.join()
+    dfs.append(df_1)
 
+df_2 = pd.concat(dfs)
+print(len(threads))
 
-print(len(df))
-df.head()
+print(len(df_2))
 
-df["translated_text"] = df["translated_text"].apply(lambda x: " ".join(x))
-df.head()
+# df_2['translated_text'] = df_2['translated_text'].apply(lambda x: ' '.join(str(x)))
 
 # remove setences column
-df = df.drop(columns=["sentences"])
-df.head()
+df_2 = df_2.drop(columns=['sentences'])
 
-df.to_csv("results-publico-translated-test.csv", index=False)
+df_2.to_csv("results-publico-translated.csv", index=False)
